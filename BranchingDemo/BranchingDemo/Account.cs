e9485c7de1a142a14b178a3b0993bf12e29531bf
gggustavo <garciagustavoariel@hotmail.com>
2016-10-29 20:57:09 -0300

    first commit

---
+++ BranchingDemo/BranchingDemo/Account.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace BranchingDemo
+{
+    internal class Account
+    {
+        public decimal Balance { get; private set; }
+        public bool IsVerified { get; set; }
+        public bool IsClosed { get; set; }
+        public bool IsFrozen { get; set; }
+
+        private Action OnUnfreeze { get; }
+
+        public Account(Action onUnfreeze)
+        {
+            OnUnfreeze = onUnfreeze;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (IsClosed)
+                return;
+
+            if (IsFrozen)
+            {
+                IsFrozen = false;
+                OnUnfreeze();
+            }
+            Balance += amount;//Deposit money.
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (!IsVerified)
+                return; // Or do something else...
+
+            if (IsClosed)
+                return; //// Or do something else...
+
+            if (IsFrozen)
+            {
+                IsFrozen = false;
+                OnUnfreeze();
+            }
+
+            Balance -= amount; //Withdraw money.
+        }
+
+        public void HolderVerfied()
+        {
+            IsVerified = true;
+        }
+
+        public void Close()
+        {
+            IsClosed = true;
+        }
+
+        public void Freezen()
+        {
+            if (IsClosed)
+                return; // Account must not be closed
+
+            if (!IsVerified)
+                return; // Account must be verified
+
+            IsFrozen = true;
+        }
+        
+    }
+}
