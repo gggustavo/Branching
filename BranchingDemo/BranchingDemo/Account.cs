f7755b019c6eb54e7ae7d762d115a8b2d742bd54
gggustavo <garciagustavoariel@hotmail.com>
2016-10-31 21:00:05 -0300

    first account

--- BranchingDemo/BranchingDemo/Account.cs
+++ BranchingDemo/BranchingDemo/Account.cs
@@ -5,37 +5,66 @@ namespace BranchingDemo
     internal class Account
     {
         public decimal Balance { get; private set; }
+        public bool IsVerified { get; set; }
+        public bool IsClosed { get; set; }
+        public bool IsFrozen { get; set; }
 
-        private IAccountState State { get; set; }
+        private Action OnUnfreeze { get; }
 
         public Account(Action onUnfreeze)
         {
-            State = new NotVerified(onUnfreeze);
+            OnUnfreeze = onUnfreeze;
         }
 
         public void Deposit(decimal amount)
         {
-            State = State.Deposit(() => { Balance += amount; });
+            if (IsClosed)
+                return;
+
+            if (IsFrozen)
+            {
+                IsFrozen = false;
+                OnUnfreeze();
+            }
+            Balance += amount;//Deposit money.
         }
 
         public void Withdraw(decimal amount)
         {
-            State = State.Withdraw(() => { Balance -= amount; });
+            if (!IsVerified)
+                return; // Or do something else...
+
+            if (IsClosed)
+                return; //// Or do something else...
+
+            if (IsFrozen)
+            {
+                IsFrozen = false;
+                OnUnfreeze();
+            }
+
+            Balance -= amount; //Withdraw money.
         }
 
         public void HolderVerfied()
         {
-            State = State.HolderVerified();
+            IsVerified = true;
         }
 
         public void Close()
         {
-            State = State.Close();
+            IsClosed = true;
         }
 
         public void Freezen()
         {
-            State = State.Freeze();
+            if (IsClosed)
+                return; // Account must not be closed
+
+            if (!IsVerified)
+                return; // Account must be verified
+
+            IsFrozen = true;
         }
 
     }
